4a5c8445102512f1eef2630977e6259f3a6fad87
Adrian A. Baumann <ade@adebaumann.com>
2022-05-26 00:38:46 +0200

    DoubleReset now counts (and returns) the number of resets within the specified time (default 5 seconds)

--- aabiot/DoubleReset.py
+++ aabiot/DoubleReset.py
@@ -11,9 +11,12 @@ def DoubleReset(Time=5,File="doublereset.txt"):
         a=open(File)
         Check=a.read().strip()
         a.close()
-        if Check == '1':
-            os.remove(File)
-            return True
+        a=open(File,"w")
+        a.write(str(int(Check)+1))
+        a.close()
+        sleep(Time)
+        os.remove(File)
+        return int(Check)+1
     except OSError:
         pass
     a=open(File,"w")
