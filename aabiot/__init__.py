558f7487599588ea77cb1e6da39004217cff3993
Adrian A. Baumann <ade@adebaumann.com>
2025-04-15 14:39:08 +0200

    Renamed files to fit coding scheme

--- aabiot/__init__.py
+++
@@ -1,4 +0,0 @@
-from aabiot.LED import LED
-from aabiot.DoubleReset import DoubleReset
-
-print("AABIOT v0.1 imported")
---
+++ mipyiot/__init__.py
@@ -0,0 +1,4 @@
+from mipyiot.led import LED
+from mipyiot.doublereset import DoubleReset
+
+#print("AABIOT v0.1 imported")
